ecf583a8ce1bab988202c745926f9e91882f7860
omar <mohammedomar.bah@gmail.com>
2026-01-30 04:28:40 +0530

    test

--- app.py
+++ app.py
@@ -1,16 +1,4 @@
 import streamlit as st
-from ratios import calculate_ratios
 
-st.set_page_config(page_title="Financial Ratio Analyzer", layout="centered")
-
-st.title("📊 Financial Ratio Analyzer")
-
-ticker = st.text_input("Enter stock ticker (e.g. AAPL, MSFT, TSLA (use .NS for indian companies)):")
-
-if ticker:
-    try:
-        df = calculate_ratios(ticker.upper())
-        st.success("Data fetched successfully")
-        st.dataframe(df, use_container_width=True)
-    except Exception as e:
-        st.error(f"Error: {e}")
+st.title("STREAMLIT IS WORKING")
+st.write("If you see this, Streamlit is fine.")
